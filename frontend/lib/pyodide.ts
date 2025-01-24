3d720e3138fa6545310ac659f9e16f1cedd2bd70
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-01-24 18:29:17 +0530

    improved function calling python

--- frontend/lib/pyodide.ts
+++ frontend/lib/pyodide.ts
@@ -2,8 +2,7 @@ import { loadPyodide, PyodideInterface } from 'pyodide';
 
 let pyodideInstance: PyodideInterface | null = null;
 
-// URL of the Python module file
-const pythonModuleURL = '/_creator.py';
+const pythonModuleURL = '/module.py';
 
 async function fetchPythonCode(url: string): Promise<string> {
   const response = await fetch(url);
@@ -29,35 +28,26 @@ export async function initializePyodide() {
 }
 
 
-export async function callPythonFunction(functionName: string, args:any): Promise<any> {
+export async function callPythonFunction(functionName: string, ...args:any[]): Promise<any> {
   const pyodide = await initializePyodide();
   try {
-    // console.log("Calling Python function:", functionName);
     const pythonFunction = pyodide.globals.get(functionName);
-    // console.log('Python function:', pythonFunction, 'type:', typeof pythonFunction);
     
     if (!pythonFunction || typeof pythonFunction !== 'function') {
       console.error('Available globals:', Object.keys(pyodide.globals.toJs()));
       throw new Error(`Function ${functionName} is not a valid Python function`);
     }
-    // if (!pythonFunction) {
-    //   console.error(`Function ${functionName} not found. Available functions:`, pyodide.globals.get('dir')());
-    //   throw new Error(`Function ${functionName} not found in Python module`);
-    // }
-    console.log("hello")
+
+    let pyArgs: any[] = [];
+
+    pyArgs = args.map((arg, index) => {
+      console.log(`Converting argument ${index}`);
+      return pyodide.toPy(arg);
+    });
+
+    const result = pythonFunction(...pyArgs);
+    return result.toJs();
     
-    // // Convert JavaScript objects to Python objects
-    // const pyTimeTable = pyodide.toPy(args.time_table_json);
-    // // console.log('pyTimeTable:', pyTimeTable);
-    // const pySubjects = pyodide.toPy(args.subject_json);
-    // // console.log('pySubjects:', pySubjects);
-    // const pyBatch = pyodide.toPy(args.batch);
-    // // console.log('pyBatch:', pyBatch);
-    // const pyElectives = pyodide.toPy(args.electives_subject_codes);
-    // // console.log('pyElectives:', pyElectives);
-    
-    // const result = pythonFunction(pyTimeTable, pySubjects, pyBatch, pyElectives);
-    // return result.toJs();
   } catch (error) {
     console.error('Error calling Python function:', error);
     throw error;
